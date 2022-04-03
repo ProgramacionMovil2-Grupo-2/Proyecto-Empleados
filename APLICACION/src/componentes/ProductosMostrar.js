06db3ceca73dc9006592445e1aa9095582704139
Sandra Calderon <mejiafrank901@gmail.com>
2022-04-03 17:30:45 -0600

    PRODUCTOSMOSTRAR

---
+++ APLICACION/src/componentes/ProductosMostrar.js
@@ -0,0 +1,318 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Pressable, FlatList, ScrollView, StatusBar, ImageComponent, Image, TextInput, Alert, Button } from 'react-native';
+//import AsyncStorage from '@react-native-async-storage/async-storage';
+import { withTheme, SearchBar } from 'react-native-elements';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import "react-native-gesture-handler";
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function registro({ navigation }) {
+  const [info, setinfo] = useState([]);
+  const [ejecucion, setEjecucion] = useState(null);
+  const [id, setid] = useState(null);
+  const [descripcion, setdescripcion] = useState(null);
+
+  if (ejecucion == null) {
+    try {
+      const response = fetch("http://192.168.1.6:4001/api/productos/listar")
+        .then((response) => response.json())
+        .then((json) => {
+          setinfo(json);
+          console.log(json);
+        });
+      setEjecucion(false);
+    } catch (error) {
+      setEjecucion(false);
+      console.error(error);
+    }
+  }
+
+
+  return (
+    <View style={styles.contenedor}>
+      <View style={styles.contenedorTitulo}>
+        <Text style={styles.titulo}>Productos</Text>
+      </View>
+      <View>
+        <TouchableOpacity onPress={() => {
+          navigation.navigate('Buscar')
+        }}>
+          <View style={styles.back}>
+            <Feather name='arrow-left' style={{ fontSize: 25 }}
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.scrollView}>
+        <View style={styles.contenedorRegistro}>
+
+
+          <View style={[styles.contenedorControles, styles.sombraControles]}>
+            <View style={styles.controles}>
+
+
+
+              <FlatList
+                style={styles.sucursales}
+                data={info}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => {
+                  return (
+                    <Pressable
+                      style={styles.contenedorFuera}
+                    //    onPress={() => navigation.navigate("ConfirmarSucursal")}
+                    >
+                      <View style={styles.contenedorDentro}>
+                      <Text style={styles.nombre}>
+                         Nombre: {item.nombre}
+                        </Text>
+                        <Text style={styles.nombre}>
+                          Fecha V.: {item.fechavencimiento}
+                        </Text>
+                        <Text style={styles.nombre}>
+                          CodigoBarras: {item.codigobarras}
+                        </Text>
+                        <Text style={styles.nombre}>
+                          Descripcion: {item.descripcion}
+                        </Text>
+                        <Text style={styles.nombre}>
+                          Impuesto: {item.impuesto}
+                        </Text>
+                        <Text style={styles.nombre}>
+                          Precio: {item.precio}
+                        </Text>
+
+                        <Image source={{ uri: item.imagen }}
+                          style={{ width: 70, height: 70, left: 10, top: -105 }} />
+
+                        <View style={styles.circleIcon}>
+                          <TouchableOpacity onPress={() => {
+                            navigation.navigate('Buscar')
+                          }}>
+                          </TouchableOpacity>
+                        </View>
+
+                      </View>
+                    </Pressable>
+                  );
+                }}
+              />
+
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  textinput: {
+    fontSize: 18
+  },
+  textcuenta: {
+    fontSize: 16
+  },
+  contenedor: {
+    paddingTop: StatusBar.currentHeight,
+    alignItems: 'center',
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#ffffff",
+  },
+  contenedorRegistro: {
+    alignItems: "stretch",
+    justifyContent: 'center',
+    height: '100%',
+    width: '100%',
+  },
+  contenedorTitulo: {
+    backgroundColor: '#00A41F',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 170,
+    width: '100%',
+    top: 4,
+  },
+  contenedorControles: {
+    flex: 3,
+    flexDirection: "column",
+    alignItems: "stretch",
+    justifyContent: "center",
+    padding: 10,
+    top: 10,
+  },
+  contenedorLogo: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sombraControles: {
+    shadowColor: '#171717',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  titulo: {
+    color: "#FFFFFF",
+    fontSize: 30,
+    fontWeight: "700",
+    top: 40,
+  },
+  tituloBoton: {
+    color: "#FFFFFF",
+    fontSize: 14,
+    fontWeight: "500",
+  },
+  tituloBoton2: {
+    color: "#FFFFFF",
+    fontSize: 15,
+    fontWeight: "600",
+  },
+  controles: {
+    flex: 3,
+    marginBottom: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  contenedorBotones: {
+    paddingLeft: 10,
+    marginTop: 0,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    height: 100,
+  },
+  boton: {
+    alignItems: "center",
+    marginLeft: 5,
+    marginRight: 5,
+    height: '40%',
+    width: 170,
+  },
+  botonRedes: {
+    flex: 0.3,
+    alignItems: "stretch",
+    marginTop: 20,
+  },
+  entradas: {
+    flex: 0.3,
+    alignItems: "stretch",
+    margin: 5,
+    padding: 8,
+    fontSize: 16,
+    fontWeight: "400",
+    color: "#495057",
+    backgroundColor: "#fff",
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: '#FFFFFF',
+    borderRadius: 25,
+  },
+  iniciarSesion: {
+    width: 150,
+    height: 40,
+    alignContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 40,
+    backgroundColor: '#3A6C96',
+  },
+  botonGuardar: {
+    width: 225,
+    height: 50,
+    marginTop: 10,
+    alignContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 40,
+    backgroundColor: '#0D7701',
+    left: 0,
+  },
+  botonEditar: {
+    width: 225,
+    height: 50,
+    marginTop: -40,
+    alignContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 40,
+    backgroundColor: '#0D7701',
+    top: -12,
+    left: 230,
+  },
+  botonEliminar: {
+    width: '100%',
+    height: 50,
+    alignContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 40,
+    backgroundColor: '#0D7701',
+  },
+  botonBuscar: {
+    width: '100%',
+    height: 50,
+    alignContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 40,
+    backgroundColor: '#0D7701',
+    top: 10,
+  },
+  nombre: {
+    fontSize: 20,
+    left: 85,
+    top: 55,
+  },
+  sucursales: {
+    borderRadius: 10,
+  },
+  contenedorFuera: {
+    width: "100%",
+    borderWidth: 5,
+    padding: 4,
+    borderColor: "#BAFBB9",
+  },
+  circleIcon: {
+    backgroundColor: "#ffffff",
+    borderRadius: 100,
+    padding: 10,
+    position: 'relative',
+    left: 350,
+    fontSize: 10,
+    height: 45,
+    width: 45,
+    top: -15,
+  },
+
+  back: {
+    top: -50,
+    left: -210,
+  },
+  scrollView: {
+    marginHorizontal: 20,
+    width: '100%',
+    top: -25,
+    backgroundColor: '#BAFBB9',
+  },
+  contenedorDentro: {
+    backgroundColor: "#ffffff",
+  }
+});
---
+++ APLICACION/src/componentes/productos.js
@@ -0,0 +1,485 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Pressable, FlatList, ScrollView, StatusBar, ImageComponent, Image, TextInput, Alert, Button } from 'react-native';
+//import AsyncStorage from '@react-native-async-storage/async-storage';
+import { withTheme, SearchBar } from 'react-native-elements';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import "react-native-gesture-handler";
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function registro({ navigation }) {
+  const [info, setinfo] = useState([]);
+  const [ejecucion, setEjecucion] = useState(null);
+  const [id, setid] = useState(null);
+  const [nombre, setnombre] = useState(null);
+  const [fechavencimiento, setfechavencimiento] = useState(null);
+  const [codigobarras, setcodigobarras] = useState(null);
+  const [descripcion, setdescripcion] = useState(null);
+  const [impuesto, setimpuesto] = useState(null);
+  const [precio, setprecio] = useState(null);
+  const [idtipo, setidtipo] = useState(null);
+  const [imagen, setimagen] = useState(null);
+  const [estado, setestado] = useState(null);
+
+  const presGuardar = async () => {
+    if (!idtipo || !fechavencimiento || !nombre || !codigobarras || !impuesto || !precio) {
+      console.log("Escriba los datos completos");
+      Alert.alert("MEDI", "Escriba los datos completos");
+    } else {
+      try {
+        const respuesta = await fetch(
+          'http://192.168.1.6:4001/api/productos/guardarProducto', {
+          method: 'POST',
+          headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            nombre: nombre,
+            fechavencimiento: fechavencimiento,
+            codigobarras: codigobarras,
+            descripcion: descripcion,
+            impuesto: impuesto,
+            precio: precio,
+            idtipo: idtipo,
+            imagen: imagen,
+            estado: estado
+          })
+        });
+        const json = await respuesta.json();
+        console.log(json);
+        Alert.alert("MEDI", "Petición procesada");
+      } catch (error) {
+        Alert.alert("MEDI", "Petición procesada");
+        //console.error(error);
+      }
+    }
+  }
+
+  const presModificar = async () => {
+    if (!idtipo || !fechavencimiento || !nombre || !codigobarras || !impuesto || !precio) {
+      console.log("Escriba los datos completos");
+      Alert.alert("MEDI", "Escriba los datos completos");
+    } else {
+      try {
+        const respuesta = await fetch(
+          'http://192.168.1.6:4001/api/productos/modificarProducto?id=' + id, {
+          method: 'PUT',
+          headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            nombre: nombre,
+            fechavencimiento: fechavencimiento,
+            codigobarras: codigobarras,
+            descripcion: descripcion,
+            impuesto: impuesto,
+            precio: precio,
+            idtipo: idtipo,
+            imagen: imagen,
+            estado: estado
+          })
+        });
+        const json = await respuesta.json();
+        console.log(json);
+        Alert.alert("MEDI", "Petición procesada");
+      } catch (error) {
+        Alert.alert("MEDI", "Petición procesada");
+        //console.error(error);
+      }
+    }
+  }
+  const presEliminar = async () => {
+    if (!id) {
+      console.log("Escriba los datos completos");
+      Alert.alert("MEDI", "Escriba los datos completos");
+    } else {
+      try {
+        const respuesta = await fetch(
+          'http://192.168.1.6:4001/api/productos/modificarEliminar?id=' + id, {
+          method: 'PUT',
+          headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+        });
+        const json = await respuesta.json();
+        console.log(json);
+        Alert.alert("MEDI", "Petición procesada");
+      } catch (error) {
+        Alert.alert("MEDI", "Petición procesada");
+        //console.error(error);
+      }
+    }
+  }
+
+
+  const presBuscar = async () => {
+    if (!descripcion) {
+      console.log("Escriba los datos completos");
+      Alert.alert("MEDI", "Escriba los datos completos");
+    } else {
+      try {
+        const respuesta = await fetch(
+          'http://192.168.1.6:4001/api/tipos/buscarTipos?filtro=' + descripcion, {
+          method: 'GET',
+          headers: {
+            accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+        });
+        const json = await respuesta.json();
+        console.log(json);
+        Alert.alert("MEDI", "Petición procesada");
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+
+  if (ejecucion == null) {
+    try {
+      const response = fetch("http://192.168.1.6:4001/api/tipos/listar")
+        .then((response) => response.json())
+        .then((json) => {
+          setinfo(json);
+          console.log(json);
+        });
+      setEjecucion(false);
+    } catch (error) {
+      setEjecucion(false);
+      console.error(error);
+    }
+  }
+
+
+  return (
+    <SafeAreaView style={styles.contenedor}>
+      <View style={styles.contenedorTitulo}>
+        <Text style={styles.tituloLogin}>Productos</Text>
+      </View>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.contenedorRegistro}>
+
+
+          <View style={[styles.contenedorControles, styles.sombraControles]}>
+            <View style={styles.controles}>
+              <Text style={styles.textinput}>   Id:</Text>
+              <TextInput
+                value={id}
+                onChangeText={setid}
+
+                placeholder="Ej. 7"
+                style={styles.entradas}
+              >
+              </TextInput>
+              <Text style={styles.textinput}>   Nombre:</Text>
+              <TextInput
+                value={nombre}
+                onChangeText={setnombre}
+
+                placeholder="Ej. Analgesico"
+                style={styles.entradas}
+              >
+              </TextInput>
+              <Text style={styles.textinput}>   Fecha:</Text>
+              <TextInput
+                value={fechavencimiento}
+                onChangeText={setfechavencimiento}
+
+                placeholder="Ej. 2022/03/30"
+                style={styles.entradas}
+              >
+              </TextInput>
+              <Text style={styles.textinput}>   Codigo Barras:</Text>
+              <TextInput
+                value={codigobarras}
+                onChangeText={setcodigobarras}
+
+                placeholder="Ej. 01010202"
+                style={styles.entradas}
+              >
+              </TextInput>
+              <Text style={styles.textinput}>   Descripcion:</Text>
+              <TextInput
+                value={descripcion}
+                onChangeText={setdescripcion}
+
+                placeholder="Ej. Lo mejor"
+                style={styles.entradas}
+              >
+              </TextInput>
+              <Text style={styles.textinput}>   Impuesto:</Text>
+              <TextInput
+                value={impuesto}
+                onChangeText={setimpuesto}
+
+                placeholder="Ej. 15%"
+                style={styles.entradas}
+              >
+              </TextInput>
+              <Text style={styles.textinput}>   Precio:</Text>
+              <TextInput
+                value={precio}
+                onChangeText={setprecio}
+
+                placeholder="Ej. 23"
+                style={styles.entradas}
+              >
+              </TextInput>
+              <Text style={styles.textinput}>   Categoria:</Text>
+              <TextInput
+                value={idtipo}
+                onChangeText={setidtipo}
+
+                placeholder="Ej. numero"
+                style={styles.entradas}
+              >
+              </TextInput>
+              <Text style={styles.textinput}>   Imagen:</Text>
+              <TextInput
+                value={imagen}
+                onChangeText={setimagen}
+
+                placeholder="Ej. "
+                style={styles.entradas}
+              >
+              </TextInput>
+
+              <Text style={styles.textinput}>   Estado:</Text>
+              <TextInput
+                value={estado}
+                onChangeText={setestado}
+
+                placeholder="Ej. "
+                style={styles.entradas}
+              >
+              </TextInput>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+      <View style={styles.botonRedes}>
+        <Button
+          title="GUARDAR" color={"#0D7701"} onPress={presGuardar}>
+        </Button>
+      </View>
+      <View style={styles.botonRedes}>
+        <Button
+          title="MODIFICAR" color={"#3A6C96"} onPress={presModificar}>
+        </Button>
+      </View>
+      <View style={styles.botonRedes}>
+        <Button style={styles.nombreBoton}
+          title="ELIMINAR" color={"#36465d"} onPress={presEliminar}>
+        </Button>
+      </View>
+      <View style={styles.botonRedes}>
+        <Button
+          title="VER" color={"#055b5c"} onPress={presModificar}>
+        </Button>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  textinput: {
+    fontSize: 18
+  },
+  textcuenta: {
+    fontSize: 16
+  },
+  contenedor: {
+    paddingTop: StatusBar.currentHeight,
+    alignItems: 'center',
+    justifyContent: "center",
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#DEFFDA",
+  },
+  contenedorRegistro: {
+    alignItems: "stretch",
+    justifyContent: 'center',
+    height: '100%',
+    width: '100%',
+  },
+  contenedorTitulo: {
+    backgroundColor: '#00A41F',
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 80,
+    width: '100%',
+    top: -20,
+  },
+  contenedorControles: {
+    flex: 3,
+    flexDirection: "column",
+    alignItems: "stretch",
+    justifyContent: "center",
+    padding: 0,
+  },
+  contenedorLogo: {
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  sombraControles: {
+    shadowColor: '#171717',
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+  },
+  tituloLogin: {
+    color: "#FFFFFF",
+    fontSize: 23,
+    fontWeight: "700",
+  },
+  tituloBoton: {
+    color: "#FFFFFF",
+    fontSize: 14,
+    fontWeight: "500",
+  },
+  tituloBoton2: {
+    color: "#FFFFFF",
+    fontSize: 15,
+    fontWeight: "600",
+  },
+  controles: {
+    flex: 3,
+    marginBottom: 30,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  contenedorBotones: {
+    paddingLeft: 10,
+    marginTop: 0,
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    height: 100,
+  },
+  boton: {
+    alignItems: "center",
+    marginLeft: 5,
+    marginRight: 5,
+    height: '40%',
+    width: 170,
+  },
+  botonRedes: {
+    flex: 0.20,
+    alignItems: "stretch",
+    top: 0,
+    height: '100%',
+    width: '100%',
+  },
+  entradas: {
+    flex: 0.3,
+    alignItems: "stretch",
+    margin: 5,
+    padding: 8,
+    fontSize: 16,
+    fontWeight: "400",
+    color: "#495057",
+    backgroundColor: "#fff",
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: '#FFFFFF',
+    borderRadius: 25,
+  },
+  iniciarSesion: {
+    width: 150,
+    height: 40,
+    alignContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 40,
+    backgroundColor: '#3A6C96',
+  },
+  botonGuardar: {
+    width: 225,
+    height: 50,
+    marginTop: 10,
+    alignContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 40,
+    backgroundColor: '#0D7701',
+    left: 0,
+  },
+  botonEditar: {
+    width: 225,
+    height: 50,
+    marginTop: -40,
+    alignContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    borderRadius: 40,
+    backgroundColor: '#0D7701',
+    top: -12,
+    left: 230,
+  },
+  botonEliminar: {
+    width: '100%',
+    height: 50,
+    alignContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 40,
+    backgroundColor: '#0D7701',
+  },
+  botonBuscar: {
+    width: '100%',
+    height: 50,
+    alignContent: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 40,
+    backgroundColor: '#0D7701',
+    top: 10,
+  },
+  nombre: {
+    fontSize: 20,
+    left: 10,
+    top: 28,
+  },
+  sucursales: {
+    borderRadius: 10,
+  },
+  contenedorFuera: {
+    width: "100%",
+    borderWidth: 2,
+    borderColor: "#BAFBB9",
+  },
+  circleIcon: {
+    backgroundColor: "#31C02E",
+    borderRadius: 100,
+    padding: 10,
+    position: 'relative',
+    left: 350,
+    fontSize: 10,
+    height: 45,
+    width: 45,
+    top: -30,
+  },
+  scrollView: {
+    marginHorizontal: 20,
+    width: '100%',
+    top: -20,
+    height: '46%'
+  }
+});
